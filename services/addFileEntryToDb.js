77848f75757542e8cb8879902f6b31966f71cd2d
Marcin Lasecki <marcin.1993@poczta.fm>
2019-12-14 16:54:51 +0100

    Removed connect/disconnect to database while upload process

--- services/addFileEntryToDb.js
+++ services/addFileEntryToDb.js
@@ -2,12 +2,9 @@ const dbConnection = require('../db/connection');
 
 module.exports = function addFileEntryToDb({ fileId, fileName, isPrivate }) {
   return new Promise((resolve, reject) => {
-    dbConnection.connect();
-
     const sql = 'INSERT INTO `files` (`file_id`, `file_name`, `is_private`) VALUES (?, ?, ?)';
 
     dbConnection.query(sql, [fileId, fileName, isPrivate], err => {
-      dbConnection.end();
       return err ? reject(err) : resolve();
     });
   });
